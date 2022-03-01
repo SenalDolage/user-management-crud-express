91137354ad93a131776d98ee82181bc974443af0
Senal <senal.dolage@gmail.com>
2022-03-02 00:30:20 +0530

    mongodb connection added

--- server.js
+++ server.js
@@ -5,12 +5,20 @@ const path = require("path");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT;
+const connectDB = require('./server/database/connection');
 
+connectDB();
+
+/* log requests using morgan*/
 app.use(morgan("tiny"));
+
 app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use("/css", express.static(path.resolve(__dirname, "assets/css")));
 app.use("/images", express.static(path.resolve(__dirname, "assets/images")));
 app.use("/js", express.static(path.resolve(__dirname, "assets/js")));
+
+/* defining routes separately */
 app.use('/', require('./server/routes/router'));
 
 app.set("view engine", "ejs");
